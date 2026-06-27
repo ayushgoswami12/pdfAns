f2625160c51b58b42669dc1cb0dc3e5f24aa8d01
ayushgoswami12 <ayushgoswami2005ag@gmail.com>
2026-06-27 17:44:46 +0530

    new design

--- rag-frontend/app/chat/page.tsx
+++ rag-frontend/app/chat/page.tsx
@@ -324,11 +324,15 @@ export default function Home() {
               >
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="translate-x-[1px] -translate-y-[1px]">
                   <path d="m5 12 7-7 7 7"/>
+
+
+
+                  
                   <path d="M12 19V5"/>
                 </svg>
               </button>
             </div>
-            
+            {/*  */}
             {/* new  */}
             <p className="text-center text-[11px] text-zinc-600 mt-4 font-medium">
               AI can make mistakes. Consider verifying important information.
