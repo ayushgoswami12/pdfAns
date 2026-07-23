ca97eb82691b1882813c9ee3aec84f4de1e4f7d4
ayushgoswami12 <ayushgoswami2005ag@gmail.com>
2026-07-23 10:21:25 +0530

    pinecode

--- main.py
+++ main.py
@@ -1,60 +1,47 @@
 from dotenv import load_dotenv
 from langchain_mistralai import MistralAIEmbeddings
-# from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_mistralai import ChatMistralAI 
+from langchain_mistralai import ChatMistralAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_pinecone import PineconeVectorStore
+import os
 
 load_dotenv()
 
 embedding_model = MistralAIEmbeddings()
-vectorStore = Chroma(
-    persist_directory="ChromaDB" , 
-    embedding_function=embedding_model
+
+vectorStore = PineconeVectorStore(
+    index_name=os.getenv("PINECONE_INDEX_NAME"),
+    embedding=embedding_model,
 )
 
-
 retriver = vectorStore.as_retriever(
-    search_type = "mmr",
-    search_kwargs = {
-        "k":4 ,
-        'fetch_k' : 10 , #first it will fetch 10 embeddings then 4 
-        "lambda_mult" : 0.5 , #0 => more diverse , 1 => less diverse 
+    search_type="mmr",
+    search_kwargs={
+        "k": 4,
+        "fetch_k": 10,
+        "lambda_mult": 0.5,
     }
 )
 
-llm = ChatMistralAI(model= "mistral-small-2506")
-
+llm = ChatMistralAI(model="mistral-small-2506")
 
 prompt = ChatPromptTemplate.from_messages([
-    ("system","You are AI assistant , USE ONLY the provided answer the question. and if you coudnt find the answer then try to find it in the interet (saying was not from the material so it has been taken from internet )' "),
-    ("human",'''
-     Context : {context} Question : {question}
-     ''')
-    
+    ("system", "You are AI assistant, USE ONLY the provided context to answer the question. If you couldn't find the answer say 'could not find the answer in the provided material'."),
+    ("human", "Context : {context} Question : {question}")
 ])
 
-print("Rag System Created ")
-print("Press 0 to exit ")
+print("Rag System Created")
+print("Press 0 to exit")
 
 if __name__ == "__main__":
-    while True: 
-        query = input("You  : ")
-        if query == "0": 
-            break 
+    while True:
+        query = input("You: ")
+        if query == "0":
+            break
         docs = retriver.invoke(query)
-        
-        context ="\n\n".join(
-            [doc.page_content for doc in docs]
-        ) 
-        new_prompt = prompt.invoke(
-            {
-                "context": context , 
-                "question" : query
-            }
-        )
-        
+        context = "\n\n".join([doc.page_content for doc in docs])
+        new_prompt = prompt.invoke({"context": context, "question": query})
         response = llm.invoke(new_prompt)
         print(f"\n AI: {response.content}")
